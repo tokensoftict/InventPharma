9f5c99ba62e73146a9bce4f8009e57792b79a1e6
Yusuf Olatunji <yoyforever@gmail.com>
2025-09-29 11:57:01 +0100

    sync stock to online

--- app/Console/Commands/SyncStockToServer.php
+++ app/Console/Commands/SyncStockToServer.php
@@ -2,6 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\Enums\KafkaAction;
+use App\Enums\KafkaTopics;
+use App\Jobs\PushDataServer;
 use App\Models\Stock;
 use Illuminate\Console\Command;
 
@@ -28,26 +31,29 @@ class SyncStockToServer extends Command
      */
     public function handle()
     {
+        //now finally lets handle stock pushing
+        $this->info('Gathering Bulk Stock Data');
         $stocks = Stock::where(function($query){
             $query->orWhere('bulk_price','>',0)->orWhere('retail_price','>',0);
-        })->where('status',1)->orderBy('id', 'DESC');
-        $chunk_numbers = round(($stocks->count() / 500));
-        $stocks->chunk(500,function($stocks) use (&$chunk_numbers){
+        });
+        $chunk_numbers = round(($stocks->count() / 2000));
+        $stocks->chunk(2000,function($stocks) use (&$chunk_numbers){
             $all_data = [];
             foreach($stocks as $stock){
                 $all_data[] = $stock->getBulkPushData();
             }
             $this->info('Gathering Stock Data Complete');
             $this->info('Parsing Stock Data');
-            $postdata = ['table'=>'stock','data'=> $all_data];
             $this->info('Parsing Stock Data Complete');
-            $this->info('Posting Stock Data to '.onlineBase());
-            $response = _POST('bulk_add_data',$postdata);
-            $chunk_numbers = $chunk_numbers-1;
-            $this->info(json_encode($response));
-            $this->info('Stock data has been posted successfully for chunk '.$chunk_numbers);
-            sleep(3);
+            $this->info('Posting Stock Data to '.onlineBase("stocks"));
+            dispatch(new PushDataServer(['KAFKA_ACTION' => KafkaAction::CREATE_STOCK, 'KAFKA_TOPICS'=> KafkaTopics::STOCKS, 'action' => 'new',
+                'table' => 'stock', 'data' => $all_data, 'endpoint' => 'stocks', 'url'=>onlineBase()."dataupdate/add_or_update_stock"]));
+            $this->info('Chunk '. $chunk_numbers. ' send successfully');
+            $chunk_numbers --;
         });
+
+        $this->info('Data has been uploaded to server successfully');
+
         return Command::SUCCESS;
     }
 }
