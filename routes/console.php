2adf0ba4a08d421a6eece417e3f9e52d4a6b3ba2
Yusuf Olatunji <yoyforever@gmail.com>
2025-09-29 12:15:19 +0100

    activating back job

--- routes/console.php
+++ routes/console.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command("queue:work --stop-when-empty")->everyFiveSeconds()->withoutOverlapping(); //to run emails
 Schedule::command('open:stock')->dailyAt('01:00');
-Schedule::command('orders:refresh')->everyMinute()->withoutOverlapping();
+//Schedule::command('orders:refresh')->everyMinute()->withoutOverlapping();
 
 //Schedule::command('queue:work --sansdaemon --tries=3 --timeout=0')->everyMinute()->withoutOverlapping()->appendOutputTo('storage/app/queuework.txt');
 
@@ -22,4 +22,4 @@ Schedule::command('sync:stock')->everyTwoHours();
 
 //Schedule::command('download:product-image')->withoutOverlapping()->everyMinute()->appendOutputTo('storage/app/imageDownload.txt');
 
-Schedule::command('uploadproduct:image')->withoutOverlapping()->everyMinute()->appendOutputTo('storage/app/imageUpload.txt');
+//Schedule::command('uploadproduct:image')->withoutOverlapping()->everyMinute()->appendOutputTo('storage/app/imageUpload.txt');
