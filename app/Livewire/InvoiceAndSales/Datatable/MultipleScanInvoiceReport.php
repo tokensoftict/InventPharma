980434621a781999fa3acb636ff2716c2d35dc8e
Yusuf Olatunji <yoyforever@gmail.com>
2025-10-17 13:31:28 +0100

    show user name

--- app/Livewire/InvoiceAndSales/Datatable/MultipleScanInvoiceReport.php
+++ app/Livewire/InvoiceAndSales/Datatable/MultipleScanInvoiceReport.php
@@ -86,6 +86,7 @@ final class MultipleScanInvoiceReport extends PowerGridComponent
             ->add('id')
             ->add('invoice_number')
             ->add('scan_date')
+            ->add("user.name")
             ->add('scan_time')
             ->add('no_of_items')
             ->add('updated_at');
