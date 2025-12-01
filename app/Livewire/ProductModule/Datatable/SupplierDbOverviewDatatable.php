0885858b3d67b4285ea7c2d4c75fb27659d97758
Yusuf Olatunji <yoyforever@gmail.com>
2025-12-01 11:18:48 +0100

    fix column name issues

--- app/Livewire/ProductModule/Datatable/SupplierDbOverviewDatatable.php
+++ app/Livewire/ProductModule/Datatable/SupplierDbOverviewDatatable.php
@@ -33,7 +33,7 @@ class SupplierDbOverviewDatatable extends ExportDataTableComponent
     {
         return [
             Column::make("Name", "supplier.name")
-                ->format(fn($value, $row, Column $column) => $value)
+                ->format(fn($value, $row, Column $column) => $row->name)
                 ->sortable()->searchable()->sortable(),
             Column::make("Total Opening Cost", "total_opening_cost_price")
             ->format(fn($value, $row, Column $column)=> money($value))->sortable(),
