5959dcab6cb3e88320014ab379a4f94137ada217
Yusuf Olatunji <yoyforever@gmail.com>
2025-12-01 11:35:16 +0100

    adding app/Models/ProductCustomPrice.php so that custom price can be sync with server

--- app/Models/ProductCustomPrice.php
+++ app/Models/ProductCustomPrice.php
@@ -9,6 +9,7 @@ namespace App\Models;
 use App\Enums\KafkaAction;
 use App\Enums\KafkaTopics;
 use App\Jobs\PushDataServer;
+use App\Traits\ModelFilterTraits;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
@@ -23,7 +24,6 @@ use Illuminate\Database\Eloquent\Model;
  * @property int $max_qty
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
- * 
  * @property Stock $stock
  * @property User $user
  *
@@ -31,6 +31,8 @@ use Illuminate\Database\Eloquent\Model;
  */
 class ProductCustomPrice extends Model
 {
+    use ModelFilterTraits;
+
 	protected $table = 'product_custom_prices';
 
 	protected $casts = [
@@ -62,8 +64,16 @@ class ProductCustomPrice extends Model
 
     public function updateonlinePush()
     {
-        if(($this->bulk_price > 0 || $this->retail_price > 0)  && !$this->isDirty('batched')) {
+        if(($this->stock->bulk_price > 0 || $this->stock->retail_price > 0)) {
             dispatch(new PushDataServer(['KAFKA_ACTION' => KafkaAction::UPDATE_STOCK, 'KAFKA_TOPICS'=> KafkaTopics::STOCKS, 'action' => 'update', 'table' => 'stock', 'data' => $this->stock->getBulkPushData(), 'endpoint' => 'stocks', 'url'=>onlineBase()."dataupdate/add_or_update_stock"]));
         }
     }
+
+    public function newonlinePush()
+    {
+        if(($this->stock->bulk_price > 0 || $this->stock->retail_price > 0)) {
+            dispatch(new PushDataServer(['KAFKA_ACTION' => KafkaAction::UPDATE_STOCK, 'KAFKA_TOPICS'=> KafkaTopics::STOCKS, 'action' => 'update', 'table' => 'stock', 'data' => $this->stock->getBulkPushData(), 'endpoint' => 'stocks', 'url'=>onlineBase()."dataupdate/add_or_update_stock"]));
+        }
+    }
+
 }
