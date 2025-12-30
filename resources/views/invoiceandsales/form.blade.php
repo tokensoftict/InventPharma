acd4d19cbb07ca5b7427b75641c4f55962031b7e
Yusuf Olatunji <yoyforever@gmail.com>
2025-12-30 13:26:05 +0100

    fix increment buttons on input

--- resources/views/invoiceandsales/form.blade.php
+++ resources/views/invoiceandsales/form.blade.php
@@ -77,6 +77,18 @@
             text-decoration: line-through;
         }
 
+        /* Chrome, Safari, Edge, Opera */
+        input::-webkit-outer-spin-button,
+        input::-webkit-inner-spin-button {
+            -webkit-appearance: none;
+            margin: 0;
+        }
+
+        /* Firefox */
+        input[type=number] {
+            -moz-appearance: textfield;
+        }
+
     </style>
 
 @endsection
