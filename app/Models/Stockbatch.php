00fb892caba9ccd8cd3e6ef67cd7028d3ec90f12
Yusuf Olatunji <yoyforever@gmail.com>
2026-03-15 16:45:07 +0100

    fix stock batch issue

--- app/Models/Stockbatch.php
+++ app/Models/Stockbatch.php
@@ -115,7 +115,6 @@ class Stockbatch extends Model
 
     public function updateonlinePush()
     {
-        dd("yes update");
         if(($this->stock->bulk_price > 0 || $this->stock->retail_price > 0)) {
             dispatch(new PushDataServer(['KAFKA_ACTION' => KafkaAction::UPDATE_STOCK, 'KAFKA_TOPICS'=> KafkaTopics::STOCKS, 'action' => 'update', 'table' => 'stock', 'data' => $this->stock->getBulkPushData(), 'endpoint' => 'stocks', 'url'=>onlineBase()."dataupdate/add_or_update_stock"]));
         }
@@ -124,7 +123,6 @@ class Stockbatch extends Model
 
     public function newonlinePush()
     {
-        dd("yes new push");
         if(($this->stock->bulk_price > 0 || $this->stock->retail_price > 0)) {
             dispatch(new PushDataServer(['KAFKA_ACTION' => KafkaAction::UPDATE_STOCK, 'KAFKA_TOPICS'=> KafkaTopics::STOCKS, 'action' => 'update', 'table' => 'stock', 'data' => $this->stock->getBulkPushData(), 'endpoint' => 'stocks', 'url'=>onlineBase()."dataupdate/add_or_update_stock"]));
         }
