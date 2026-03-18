1858a9338e373401daa44fc6d75d0185d8586ad2
Yusuf Olatunji <yoyforever@gmail.com>
2026-03-18 12:02:36 +0100

    hmm

--- app/Console/Commands/SendOrderUpdateToServer.php
+++ app/Console/Commands/SendOrderUpdateToServer.php
@@ -51,6 +51,8 @@ class SendOrderUpdateToServer extends Command
                 } else if($invoice->status_id == status('Dispatched')){
                     $this->info("processing Dispatched invoice #" . $invoice->id);
                     ProcessOrderService::sendBackOrderDispatchedMessage($invoice->onliner_order_id, NULL, $invoice->carton_no);
+                } else {
+                    $this->info("current status invoice id " . $invoice->status->name);
                 }
             }
         });
