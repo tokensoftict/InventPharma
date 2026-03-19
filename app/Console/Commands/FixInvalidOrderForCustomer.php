8bbdc29ed1bfe4e9bfad08d28d0223b5b0d4b067
Yusuf Olatunji <yoyforever@gmail.com>
2026-03-19 13:28:21 +0100

    hmm

--- app/Console/Commands/FixInvalidOrderForCustomer.php
+++ app/Console/Commands/FixInvalidOrderForCustomer.php
@@ -32,33 +32,34 @@ class FixInvalidOrderForCustomer extends Command
     public function handle()
     {
 
-        $startDay = "2026-03-17";
-        $endDay = "2026-03-18";
+        $startDay = "2026-03-16";
+        $endDay = "2026-03-17";
 
-        $in =Invoice::query()->whereBetween("invoice_date", [$startDay, $endDay])
-            ->where("department", "retail")
-            ->where('created_by', 1)
+        $in =Invoice::query()->where("invoice_date", $startDay)
+//            ->where("department", "retail")
+//            ->where('created_by', 1)
+            ->where('customer_id', 2151)
             ->whereNotNull("onliner_order_id")
             ->where('status_id', status('Draft'));
 
         $this->info("found total invoice ". $in->count());
 
         $in->chunk(100, function ($invoices) {
-                foreach ($invoices as $invoice) {
-                    $this->info("processing invoice #" . $invoice->id);
-                   DB::transaction(function () use ($invoice) {
-                       $order = Http::get("https://pa.psgdc.store/?order_id=$invoice->onliner_order_id")->json();
-                       $invoice->invoice_date = (new Carbon($order['order_date']))->format('Y-m-d');
-                       if($order['customer_type'] == "App\\Models\\SupermarketUser") {
-                           $invoice->status_id = status('Complete');
-                       } else {
-                           $invoice->status_id = status('Dispatched');
-                       }
-                       $invoice->save();
+            foreach ($invoices as $invoice) {
+                $this->info("processing invoice #" . $invoice->id);
+                DB::transaction(function () use ($invoice) {
+                    $order = Http::get("https://pa.psgdc.store/?order_id=$invoice->onliner_order_id")->json();
+                    $invoice->invoice_date = (new Carbon($order['order_date']))->format('Y-m-d');
+                    if($order['customer_type'] == "App\\Models\\SupermarketUser") {
+                        $invoice->status_id = status('Complete');
+                    } else {
+                        $invoice->status_id = status('Dispatched');
+                    }
+                    $invoice->save();
 
-                       ProcessOrderService::sendBackOrderDispatchedMessage($order['id'], $order['local_order_id'], $invoice->carton_no);
-                   });
-                }
-            });
+                    ProcessOrderService::sendBackOrderDispatchedMessage($order['id'], $order['local_order_id'], $invoice->carton_no);
+                });
+            }
+        });
     }
 }
