3323afaace0f5788b9eb6a90ca65412b7ce90fab
Yusuf Olatunji <yoyforever@gmail.com>
2026-04-10 12:12:18 +0100

    improving user exp

--- app/Livewire/Customer/Datatable/CustomerDataTable.php
+++ app/Livewire/Customer/Datatable/CustomerDataTable.php
@@ -21,7 +21,7 @@ class CustomerDataTable extends ExportDataTableComponent
 
     public function builder(): Builder
     {
-        return  Customer::query()->select('*')->with(['creditpaymentlog','invoice', 'payment', 'memberGroup', 'retailMemberGroup'])->filterdata($this->filters);
+        return Customer::query()->select('*')->with(['creditpaymentlog', 'invoice', 'payment', 'memberGroup', 'retailMemberGroup'])->filterdata($this->filters);
     }
 
     public function refresh($what)
@@ -29,19 +29,19 @@ class CustomerDataTable extends ExportDataTableComponent
 
     }
 
-    public static function mountColumn() : array
+    public static function mountColumn(): array
     {
         return [
             Column::make("Name", 'firstname')
-                ->format(fn($value,$row, Column $column) => $row->firstname.' '.$row->lastname)
+                ->format(fn($value, $row, Column $column) => $row->firstname . ' ' . $row->lastname)
                 ->sortable()->searchable(),
             Column::make("Email", "email")
                 ->sortable()->searchable(),
             Column::make("Status", "status")
-                ->format(function($value, $row, Column $column){
+                ->format(function ($value, $row, Column $column) {
                     return '  <div class="form-check form-switch mb-3" dir="ltr">
-                                    <input wire:change="toggle('.$row->id .')" id="user'.$row->id.'" type="checkbox" class="form-check-input" id="customSwitch1" '.($value ? 'checked' : '').'>
-                                    <label class="form-check-label" for="customSwitch1">'.$value ? 'Active' : 'Inactive' .'</label>
+                                    <input wire:change="toggle(' . $row->id . ')" id="user' . $row->id . '" type="checkbox" class="form-check-input" id="customSwitch1" ' . ($value ? 'checked' : '') . '>
+                                    <label class="form-check-label" for="customSwitch1">' . $value ? 'Active' : 'Inactive' . '</label>
                                 </div>';
                 })->html()
                 ->sortable(),
@@ -61,35 +61,37 @@ class CustomerDataTable extends ExportDataTableComponent
                 ->format(fn($value, $row, Column $column) => money($value))
                 ->sortable(),
             Column::make("Last Payment Date", "id")
-                ->format(function($value, $row, Column $column){
+                ->format(function ($value, $row, Column $column) {
                     $date = isset($row->payment->payment_date) ? $row->payment->payment_date : false;
                     return $date ? convert_date($date) : "N/A";
                 }),
             Column::make("Last Credit Payment Date", "id")
-                ->format(function($value, $row, Column $column){
+                ->format(function ($value, $row, Column $column) {
                     $date = isset($row->creditpaymentlog->payment_date) ? $row->creditpaymentlog->payment_date : false;
                     return $date ? convert_date($date) : "N/A";
                 }),
-                 Column::make("Last Invoice Date", "id")
-                   ->format(function($value, $row, Column $column){
-                       $date = isset($row->invoice->invoice_date) ? $row->invoice->invoice_date : false;
-                       return $date ? convert_date($date) : "N/A";
-                   })
+            Column::make("Last Invoice Date", "id")
+                ->format(function ($value, $row, Column $column) {
+                    $date = isset($row->invoice->invoice_date) ? $row->invoice->invoice_date : false;
+                    return $date ? convert_date($date) : "N/A";
+                })
                 ->sortable(),
             Column::make("Loyalty Point", "loyalty_points")
                 ->sortable(),
+            Column::make("Retail Loyalty Point", "retail_loyalty_points")
+                ->sortable(),
 
-            Column::make("Action","id")
-                ->format(function($value, $row, Column $column) {
+            Column::make("Action", "id")
+                ->format(function ($value, $row, Column $column) {
                     $html = "No Action";
 
-                    if (userCanView('customer.update')){
+                    if (userCanView('customer.update')) {
 
-                        $html=' <a class="btn btn-outline-primary btn-sm edit" wire:click="edit('.$value.')" href="javascript:void(0);" >
+                        $html = ' <a class="btn btn-outline-primary btn-sm edit" wire:click="edit(' . $value . ')" href="javascript:void(0);" >
 
-                                        <span wire:loading wire:target="edit('.$value .')" class="spinner-border spinner-border-sm me-2" role="status"></span>
+                                        <span wire:loading wire:target="edit(' . $value . ')" class="spinner-border spinner-border-sm me-2" role="status"></span>
 
-                                        <i wire:loading.remove wire:target="edit('.$value.')" class="fas fa-pencil-alt"></i>
+                                        <i wire:loading.remove wire:target="edit(' . $value . ')" class="fas fa-pencil-alt"></i>
 
                                     </a>';
                     }
