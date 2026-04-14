f980174132ff06c9351222715a54985305be4302
Yusuf Olatunji <yoyforever@gmail.com>
2026-04-14 17:59:09 +0100

    institution

--- app/Console/Commands/SyncAllStaffToKafka.php
+++ app/Console/Commands/SyncAllStaffToKafka.php
@@ -34,10 +34,8 @@ class SyncAllStaffToKafka extends Command
 
         User::query()->chunk(100, function ($users) {
             foreach ($users as $user) {
-                dd($user);
-//dd($user->getBulkPushData());
-//                dispatch(new PushDataServerNoQueue(['KAFKA_ACTION'=>KafkaAction::SYNC_STAFF, 'KAFKA_TOPICS'=> KafkaTopics::GENERAL,
-//                    'action'=>'new','table'=>'staffs', 'endpoint' => 'staffs' ,'data'=>$user->getBulkPushData]));
+                dispatch(new PushDataServerNoQueue(['KAFKA_ACTION'=>KafkaAction::SYNC_STAFF, 'KAFKA_TOPICS'=> KafkaTopics::GENERAL,
+                    'action'=>'new','table'=>'staffs', 'endpoint' => 'staffs' ,'data'=>$user->getBulkPushData()]));
             }
             $this->info('Synced ' . $users->count() . ' users...');
         });
