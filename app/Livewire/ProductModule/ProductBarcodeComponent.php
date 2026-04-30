c4d6f32ead1567d3d7eb283437dcfbd6618afd7a
Yusuf Olatunji <yoyforever@gmail.com>
2026-05-01 00:03:02 +0100

    reduced bar code to 3 digit

--- app/Livewire/ProductModule/ProductBarcodeComponent.php
+++ app/Livewire/ProductModule/ProductBarcodeComponent.php
@@ -31,7 +31,7 @@ class ProductBarcodeComponent extends Component
     {
         if (isset($this->product)) {
             // Generate a 12-digit random barcode based on product ID
-            $code = mt_rand(1000000000, 9999999999);
+            $code = mt_rand(100000, 999999);
             
             $barcode = new \App\Models\Stockbarcode([
                 'barcode' => $code,
